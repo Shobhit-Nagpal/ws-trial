080f7970d67d6d26e02aec23b811c69b7e4191d5
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-12-19 17:37:04 +0530

    create: simple websocket server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-import WebSocket from "ws";
+import WebSocket, { WebSocketServer } from "ws";
+import http from "http";
 
-console.log("Websocket time")
+const server = http.createServer((req, res) => {
+  console.log(new Date() + " Received req for: " + req.url);
+  res.end("Hi");
+});
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+
+
+  ws.send("YOoooooooooo")
+});
+
+server.listen(3000, () => {
+  console.log("Started server on port", 3000)
+})
